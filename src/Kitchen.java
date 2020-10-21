b7b16b28a9b306cc4f5ce25414d9d0588353cca7
Дмитрий Ляпунов (dmitry.lapynov) <dmitry.lapynov@corp.mail.ru>
2020-10-22 01:49:01 +0300

    with stir demo stuff

--- src/Kitchen.java
+++ src/Kitchen.java
@@ -2,6 +2,7 @@ public class Kitchen {
     public void cookPasta() {
         System.out.println("Вскипятить воду");
         System.out.println("Положить в воду макароны");
+        System.out.println("Помешать"); //Новая строка
         System.out.println("Подождать");
         System.out.println("Слить воду");
     }
