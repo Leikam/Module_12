0e8ff1ddb010f50a309d51bfe0b27af4a7efdad0
Дмитрий Ляпунов (dmitry.lapynov) <dmitry.lapynov@corp.mail.ru>
2020-10-25 00:08:12 +0300

    Runtime added

--- src/Main_12.java
+++ src/Main_12.java
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 import tasks.Code;
 import tasks.DragAndDrop;
 import tasks.Task;
@@ -6,36 +10,49 @@ import tasks.Test;
 public class Main_12 {
 
     public static void main(String... args) {
+        Calendar calendar = Calendar.getInstance();
 
         /* создаем задачи */
         Task[] tasks = new Task[40];
         for (int i =0; i < tasks.length; i++) {
-            int rest = i % 3;
-            if (rest == 0) {
-                tasks[i] = new Code();
-            } else if (rest == 1) {
-                tasks[i] = new DragAndDrop();
-            } else {
-                tasks[i] = new Test();
+            switch (RandomUtils.randomBetween(1, 3)) {
+                case 1 -> tasks[i] = new Code();
+                case 2 -> tasks[i] = new DragAndDrop();
+                case 3 -> tasks[i] = new Test();
             }
+            tasks[i].setNumber(i);
         }
 
-        /* создаем пару менторов */
-        Mentor mentor_vasya = new Mentor("Василий Петрович", 35);
-        Mentor mentor_zhanna = new Mentor("Жанна Петрович", 29);
+        /* создаем несколько менторов */
+        Mentor[] mentors = new Mentor[] {
+            new Mentor("Василий Петрович", 35),
+            new Mentor("Жанна Петрович", 29),
+            new Mentor("просто Петя", 23),
+        };
 
-        /* создаем пул студентов */
-        Student[] workingClass = new Student[20];
+        /* создаем пул студентов c именами в честь месяца */
+        Student[] workingClass = new Student[21];
         for (int i = 0; i < workingClass.length; i++) {
-            workingClass[i] = new Student("1", Utils.randomBetween(12, 101), mentor_vasya, false);
+            calendar.set(Calendar.MONTH, RandomUtils.randomIntInBounds(11));
+            workingClass[i] = new Student(
+                getStudentName(calendar),
+                RandomUtils.randomBetween(12, 101),
+                mentors[RandomUtils.randomIntInBounds(2)],
+                false
+            );
         }
 
         for (Student student : workingClass) {
-            student.resolveTasks(Utils.randomIntInBounds(tasks.length), tasks);
+            student.resolveTasks(RandomUtils.randomIntInBounds(tasks.length), tasks);
         }
 
-        System.out.print("\n\n\n=== КОНЕЦ ===");
-        System.out.printf("\n=== Всего класс порешал %d задач ===\n", Student.resolvedTaskCounterByWorkingClass);
+        System.out.printf("\n\n=== Всего класс порешал %d задач ===\n\n", Student.resolvedTaskCounterByWorkingClass);
+        System.out.print("=== КОНЕЦ ===");
+    }
+
+    private static String getStudentName(Calendar calendar) {
+        String name = new SimpleDateFormat("MMMM", new Locale("RU")).format(calendar.getTime());
+        return String.valueOf(name.charAt(0)).toUpperCase() + name.substring(1);
     }
 
 }
